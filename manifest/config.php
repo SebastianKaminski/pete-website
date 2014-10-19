eaafc5e27d3bacb5371b02bac6c2928291060c1a
Sebastian Kaminski <sebastian@castrads.com>
2014-10-19 12:35:05 +0100

    Symphony cms
    clean installation

---
+++ manifest/config.php
@@ -0,0 +1,100 @@
+<?php
+	$settings = array(
+
+
+		###### ADMIN ######
+		'admin' => array(
+			'max_upload_size' => '5242880',
+		),
+		########
+
+
+		###### SYMPHONY ######
+		'symphony' => array(
+			'admin-path' => 'admin',
+			'pagination_maximum_rows' => '20',
+			'association_maximum_rows' => '5',
+			'lang' => 'en',
+			'pages_table_nest_children' => 'no',
+			'version' => '2.5.0',
+			'cookie_prefix' => 'sym-',
+			'session_gc_divisor' => '10',
+			'cell_truncation_length' => '75',
+			'enable_xsrf' => 'yes',
+		),
+		########
+
+
+		###### LOG ######
+		'log' => array(
+			'archive' => '1',
+			'maxsize' => '102400',
+		),
+		########
+
+
+		###### DATABASE ######
+		'database' => array(
+			'host' => 'localhost',
+			'port' => '3306',
+			'user' => 'u480608763_admin',
+			'password' => 'UNBpsdg2PO',
+			'db' => 'u480608763_sym',
+			'tbl_prefix' => 'sym_',
+		),
+		########
+
+
+		###### PUBLIC ######
+		'public' => array(
+			'display_event_xml_in_source' => 'no',
+		),
+		########
+
+
+		###### GENERAL ######
+		'general' => array(
+			'sitename' => 'Symphony CMS',
+			'useragent' => 'Symphony/2.5.0',
+		),
+		########
+
+
+		###### FILE ######
+		'file' => array(
+			'write_mode' => '0644',
+		),
+		########
+
+
+		###### DIRECTORY ######
+		'directory' => array(
+			'write_mode' => '0755',
+		),
+		########
+
+
+		###### REGION ######
+		'region' => array(
+			'time_format' => 'g:i a',
+			'date_format' => 'm/d/Y',
+			'datetime_separator' => ' ',
+			'timezone' => 'Europe/London',
+		),
+		########
+
+
+		###### CACHE_DRIVER ######
+		'cache_driver' => array(
+			'default' => 'database',
+		),
+		########
+
+
+		###### IMAGE ######
+		'image' => array(
+			'cache' => '1',
+			'quality' => '90',
+		),
+		########
+	);
