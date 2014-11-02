4312ae7cdd5728e0b3ed0cf2396afef114df12d9
SebastianKaminski <sebekk161@gmail.com>
2014-11-02 16:15:38 +0000

    top menu

--- app/code/local/Mage/Page/Block/Html/Topmenu/Custom.php
+++ app/code/local/Mage/Page/Block/Html/Topmenu/Custom.php
@@ -1,66 +1,6 @@
 <?php
 	class Mage_Page_Block_Html_Topmenu_Custom extends Mage_Page_Block_Html_Topmenu
 	{
-
-
-	  // protected function _getHtml(Varien_Data_Tree_Node $menuTree, $childrenWrapClass)
-	  //   {
-	  //       $html = '';
-
-	  //       $children = $menuTree->getChildren();
-	  //       $parentLevel = $menuTree->getLevel();
-	  //       $childLevel = is_null($parentLevel) ? 0 : $parentLevel + 1;
-
-	  //       $counter = 1;
-	  //       $childrenCount = $children->count();
-
-	  //       $parentPositionClass = $menuTree->getPositionClass();
-	  //       $itemPositionClassPrefix = $parentPositionClass ? $parentPositionClass . '-' : 'nav-';
-
-	  //       foreach ($children as $child) {
-
-	  //           $child->setLevel($childLevel);
-	  //           $child->setIsFirst($counter == 1);
-	  //           $child->setIsLast($counter == $childrenCount);
-	  //           $child->setPositionClass($itemPositionClassPrefix . $counter);
-
-	  //           $outermostClassCode = '';
-	  //           $outermostClass = $menuTree->getOutermostClass();
-
-	  //           if ($childLevel == 0 && $outermostClass) {
-
-	  //               $outermostClassCode = ' class="' . $outermostClass . ($child->hasChildren() ? ' dropdown-toggle" data-hover="dropdown" ' : '"');
-
-	  //               $child->setClass($outermostClass);
-	  //           }
-
-	  //           $html .= '<li ' . $this->_getRenderedMenuItemAttributes($child) . '>';
-	  //           $html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . $dataDropdown . '><span>'
-	  //               . $this->escapeHtml($child->getName()) . '</span></a>';
-
-	  //           if ($child->hasChildren()) {
-	  //               if (!empty($childrenWrapClass)) {
-	  //                   $html .= '<div class="' . $childrenWrapClass . '">';
-	  //               }
-
-	  //              if($childLevel < 1) {
-	  //                   $html .= '<ul class="level' . $childLevel . ' dropdown-menu">';
-	  //                   $html .= $this->_getHtml($child, $childrenWrapClass);
-	  //                   $html .= '</ul>';
-	  //               }
-
-	  //               if (!empty($childrenWrapClass)) {
-	  //                   $html .= '</div>';
-	  //               }
-	  //           }
-	  //           $html .= '</li>';
-
-	  //           $counter++;
-	  //       }
-
-	  //       return $html;
-	  //   }
-
 	    /**
 	     * Returns array of menu item's classes
 	     *
@@ -75,7 +15,7 @@
 	        $classes[] = $item->getPositionClass();
 
 	        if ($item->getIsFirst()) {
-	            $classes[] = 'first';
+	            $classes[] = '';
 	        }
 
 	        if ($item->getIsActive()) {
@@ -83,7 +23,7 @@
 	        }
 
 	        if ($item->getIsLast()) {
-	            $classes[] = 'last';
+	            $classes[] = '';
 	        }
 
 	        if ($item->getClass()) {
@@ -134,7 +74,7 @@
 	                if (!empty($childrenWrapClass)) {
 	                    $html .= '<div class="' . $childrenWrapClass . '">';
 	                }
-	                $html .= '<ul class="level' . $childLevel . ' dropdown-menu" role="menu">';
+	                $html .= '<ul class="dropdown-menu" role="menu">';
 	                $html .= $this->_getHtml($child, $childrenWrapClass);
 	                $html .= '</ul>';
 
