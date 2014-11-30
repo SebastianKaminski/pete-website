611453705359cb58ae0d891a3f984738999a2868
SebastianKaminski <sebekk161@gmail.com>
2014-11-30 17:03:28 +0000

    menu

--- skin/frontend/peteshop/default-grunt/js/build/production.js
+++ skin/frontend/peteshop/default-grunt/js/build/production.js
@@ -10,9 +10,21 @@
     "use strict";
 
     $(document).ready(function(){
+    	// Search
     	$("#show-search-form").on("click", function (e) {
     		e.preventDefault();
     		$("#search_mini_form").toggleClass('hidden');
     	});
+
+    	// Menu
+		var nav = $('.navbar-fixed-top');
+		
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 136) {
+				nav.css("top", 0);
+			} else {
+				nav.css("top", "160px");
+			}
+		});    	
     });
 })(jQuery);
