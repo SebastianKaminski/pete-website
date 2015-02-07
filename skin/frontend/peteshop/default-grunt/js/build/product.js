67ae0fcdbf91d42a98d670cdecddf7316574bc70
SebastianKaminski <sebastian@castrads.com>
2015-02-07 13:21:12 +0000

    change selecte

--- skin/frontend/peteshop/default-grunt/js/build/product.js
+++ skin/frontend/peteshop/default-grunt/js/build/product.js
@@ -12,11 +12,6 @@
     $(document).ready(function(){
     	/* Override product zoom function */
     	Product.Zoom.prototype.initialize = function (imageEl, trackEl, handleEl, zoomInEl, zoomOutEl, hintEl) {  return false;  }
-
-    	/* Choose radiator finish */
-    	$("#choose-radiator-finish").on("change", function () {
-    		console.log("Finish has been changed:", $(this).val());
-    	})
     });
 
 })(jQuery);
