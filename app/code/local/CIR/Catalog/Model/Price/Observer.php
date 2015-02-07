c933611d7c79435602ff803a592a6f0a1a7c5570
SebastianKaminski <sebastian@castrads.com>
2015-02-07 14:25:45 +0000

    checkout

--- app/code/local/CIR/Catalog/Model/Price/Observer.php
+++ app/code/local/CIR/Catalog/Model/Price/Observer.php
@@ -14,13 +14,30 @@ class CIR_Catalog_Model_Price_Observer
 	    $p->setCustomPrice($customPrice)->setOriginalCustomPrice($customPrice);
 	}
 
-	public function catalogProductLoadAfter(Varien_Event_Observer $observer)
+	public function catalog_product_load_after(Varien_Event_Observer $observer)
 	{
 		$action = Mage::app()->getFrontController()->getAction();
 		if ($action->getFullActionName() == 'checkout_cart_add')
 		{
-			$options = $action->getRequest()->getParam('extra_options');
-			Mage:log($options);
+		    if ($options = $action->getRequest()->getParam('extra_options'))
+		    {
+		        $product = $observer->getProduct();
+
+		        $additionalOptions = array();
+		        if ($additionalOption = $product->getCustomOption('additional_options'))
+		        {
+		            $additionalOptions = (array) unserialize($additionalOption->getValue());
+		        }
+		        foreach ($options as $key => $value)
+		        {
+		            $additionalOptions[] = array(
+		                'label' => $key,
+		                'value' => $value,
+		            );
+		        }
+		        // add the additional options array with the option code additional_options
+		        $observer->getProduct()->addCustomOption('additional_options', serialize($additionalOptions));
+		    }
 		}
 	}
 }
