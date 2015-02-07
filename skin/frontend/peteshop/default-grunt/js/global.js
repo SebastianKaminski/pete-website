b91d9b39da8dad76b64c897a57cedc596b787ab7
SebastianKaminski <sebekk161@gmail.com>
2015-02-07 20:18:31 +0000

    remove links

--- skin/frontend/peteshop/default-grunt/js/global.js
+++ skin/frontend/peteshop/default-grunt/js/global.js
@@ -30,26 +30,3 @@
 		});    	
     });
 })(jQuery);
-
-function make_link(string) {
-    var words = string.split(' '),
-        ret = document.createDocumentFragment();
-    for (var i = 0, l = words.length; i < l; i++) {
-        if (words[i].match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi)) {
-            var elm = document.createElement('a');
-            elm.href = words[i];
-            elm.textContent = words[i];
-            if (ret.childNodes.length > 0) {
-                ret.lastChild.textContent += ' ';
-            }
-            ret.appendChild(elm);
-        } else {
-            if (ret.lastChild && ret.lastChild.nodeType === 3) {
-                ret.lastChild.textContent += ' ' + words[i];
-            } else {
-                ret.appendChild(document.createTextNode(' ' + words[i]));
-            }
-        }
-    }
-    return ret;
-}
