be533719e561f813f6348fac3fabd2301ba5c5b6
SebastianKaminski <sebekk161@gmail.com>
2015-02-08 13:12:44 +0000

    observers

--- app/code/local/CIR/Catalog/Model/Price/Observer.php
+++ app/code/local/CIR/Catalog/Model/Price/Observer.php
@@ -4,20 +4,22 @@ class CIR_Catalog_Model_Price_Observer
 {
 	public function apply_radiator_price($observer) {
 		$event = $observer->getEvent();
-		$product = $event->getProduct();   
+		$product = $observer->getQuoteItem();
 
-		if ($product->getSuperProduct() && $product->getSuperProduct()->isConfigurable()) {
-			// Not simple product here
-		} else {
+		// if ($product->getSuperProduct() && $product->getSuperProduct()->isConfigurable()) {
+		// 	// Not simple product here
+		// } else {
 			// Simple product
 			$params = Mage::app()->getFrontController()->getRequest()->getParams();
 			$specialPrice = $params['number-of-sections'] * ($params['radiator-finish'] + $product->getPrice());
+			$product->setOriginalCustomPrice($specialPrice);
 			$product->setFinalPrice($specialPrice);
-		}
+		// }
 
-  		Mage::log($product->debug(), null, 'debug.log', true);
+  		Mage::log("Special price = ".$specialPrice, null, 'debug.log', true);
+  		// Mage::log($product->debug(), null, 'debug.log', true);
   		Mage::log(Mage::app()->getFrontController()->getRequest()->getParams(), null, 'debug.log', true);
-  		
+
 		return $this;	
 	}
 }
