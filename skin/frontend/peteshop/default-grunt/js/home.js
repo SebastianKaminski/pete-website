9158736a8562eac7108fa97373a5fd176e0dfe33
SebastianKaminski <sebastian@castrads.com>
2015-03-14 12:49:41 +0000

    scroll

--- skin/frontend/peteshop/default-grunt/js/home.js
+++ skin/frontend/peteshop/default-grunt/js/home.js
@@ -11,6 +11,20 @@
 
     $(document).ready(function(){
     	// Script here
-    	console.log("test hp")
+    	$("#categories").slick({
+    		infinite: false,
+    		dots: false,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			responsive: [
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+			]
+    	});
     });
 })(jQuery);
